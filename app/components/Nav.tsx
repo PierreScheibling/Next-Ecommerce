0d0dd0b92fb5b9d0988804d82d50e542f7ceeab5
Pierre Scheibling <pierrescheibling@MacBook-Pro-de-Pierre.local>
2023-06-12 11:35:43 +0200

    implementing navbar

---
+++ app/components/Nav.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import {Session} from 'next-auth'
+import {signIn} from 'next-auth/react'
+import Image from 'next/image'
+
+export default function Nav({user}: Session) {
+    return(
+        <nav className="flex justify-between items-center py-8">
+            <h1>Styled</h1>
+            <ul className="flex items-center gap-12">
+                {/* If the user is not sign In */}
+                {!user && (
+                    <li className="bg-teal-600 text-white py-2 px-4">
+                        <button onClick={() => signIn()}>Sign in</button>
+                    </li>
+                )}
+                {user && (
+                    <li>
+                        <Image src={user?.image as string} alt={user.name as string} width={48} height={48} className="rounded-full" />
+                    </li>
+                )}
+            </ul>
+        </nav>
+    )
+}
