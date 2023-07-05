d51d385b4d00a588effed760817c4367a2a189c7
Pierre Scheibling <pierrescheibling@MacBook-Pro-de-Pierre.local>
2023-07-05 19:31:23 +0200

    fix bug text landing page

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -9,13 +9,16 @@ import { motion } from "framer-motion";
 export const Landing = () => {
     return (
         <AnimatePresence >
-            <motion.div className="flex-grow"
+            <motion.div className="flex-grow relative"
                 initial={{ y: 10, opacity: 0 }}
                 animate={{ y: 0, opacity: 1 }}
                 exit={{ y: -10, opacity: 0, transition: {duration: 1} }}
             >
-                <Image src={angelic} alt="landingPageImage" width={800} height={800} className="flex-grow-1 h-[80vh] w-[100%] object-cover static grayscale-[10%]"/>
-                <div className="absolute bottom-[33%] left-[20%] flex-col justify-center align-center w-[60%]">
+                <Image src={angelic} alt="landingPageImage" width={800} height={800} className="flex-grow-1 h-[80vh] w-[100%] object-cover grayscale-[10%]"/>
+                <motion.div initial={{ opacity: 0 }}
+                    animate={{ opacity: 1, transition: {delay: 0.4} }}
+                    exit={{ opacity: 0, transition: {duration: 1} }}
+                    className="absolute bottom-[33%] left-[20%] flex-col justify-center align-center w-[60%]">
                     <h1 className="font-lobster flex justify-center text-5xl my-12 text-black font-extralight">Discover our Summer Collection</h1>
                     <Link href={"/shop"} className="flex align-center justify-center">
                         <motion.div 
@@ -25,7 +28,7 @@ export const Landing = () => {
                             Shop Now
                         </motion.div>
                     </Link>
-                </div>
+                </motion.div>
                 {/* <Link href={"/"}>
                     <motion.div
                         whileHover={{ scale: 1.05 }}
