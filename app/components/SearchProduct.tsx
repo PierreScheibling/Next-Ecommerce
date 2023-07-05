92a41825d45929f3268a2ef1ccf16bbd4b348927
Pierre Scheibling <pierrescheibling@MacBook-Pro-de-Pierre.local>
2023-07-05 16:44:54 +0200

    add section details toggle

--- app/components/SearchProduct.tsx
+++ app/components/SearchProduct.tsx
@@ -5,8 +5,23 @@ import { SearchParamTypes } from "@/types/SearchParamTypes"
 import formatPrice from "@/util/PriceFormat"
 import AddCart from "../product/[id]/AddCart"
 import Image from "next/image"
+import { useState } from 'react'
+import natural from "../../public/100p-natural.png"
+import european from "../../public/european-union.png"
+import road from "../../public/road-transport.png"
 
 export const SearchProduct = ({searchParams}: SearchParamTypes) => {
+    const [selectedSize, setSelectedSize] = useState(null);
+    const [isDetailsOpen, setIsDetailsOpen] = useState(false);
+
+    const handleToggleDetails = () => {
+        setIsDetailsOpen(!isDetailsOpen);
+    };
+
+    const handleSizeClick = (size: any) => {
+        setSelectedSize(size);
+      };
+
     return(
         <AnimatePresence>
             <motion.div className="flex flex-col md:flex-row items-center md:items-start justify-between gap-12" initial={{ y: 10, opacity: 0 }}
@@ -19,7 +34,11 @@ export const SearchProduct = ({searchParams}: SearchParamTypes) => {
                     {/* <p className="py-2">{searchParams.features}</p> */}
                     <div className="py-2">
                         {Array.isArray(searchParams.size) && searchParams.size.length > 1 && searchParams.size.map((s) => (
-                            <span key={s} className="items-center mr-2 text-sm cursor-pointer text-gray-500 hover:text-black">{s}</span>
+                            <span key={s}
+                            className={`items-center mr-2 text-sm cursor-pointer ${
+                              selectedSize === s ? "text-black" : "text-gray-500"
+                            }`}
+                            onClick={() => handleSizeClick(s)}>{s}</span>
                         ))}
                     </div>
                     <div className="flex gap-2">
@@ -27,7 +46,31 @@ export const SearchProduct = ({searchParams}: SearchParamTypes) => {
                     </div>
                     <AddCart {...searchParams}/>
                     <div className="flex justify-between mt-1 text-sm"><span>Free Shipping</span><div className="h-8 border-r pr-4"></div><span>Free return</span></div>
-                    <div className="text-sm mb-4">Details</div>
+                    <div className="text-sm my-4 cursor-pointer underline" onClick={handleToggleDetails}>Details +</div>
+                    {isDetailsOpen && (
+                    <div className="flex justify-between">
+                        <div className="flex-col align-center w-[40%]">
+                            <div className="flex ml-6 align-center my-4">
+                                <Image src={natural} alt="100% natural" width={100} height={100} className="h-6 w-6"/>
+                                <p className="flex text-xs ml-4 font-thin h-6 align-center">100% natural</p>
+                            </div>
+                            <div className="flex ml-6 align-center my-4">
+                                <Image src={european} alt="European Union" width={100} height={100} className="h-6 w-6"/>
+                                <p className="flex text-xs ml-4 font-thin h-6 align-center">European Union</p>
+                            </div>
+                            <div className="flex ml-6 align-center my-4">
+                                <Image src={road} alt="Road Transport" width={100} height={100} className="h-6 w-6"/>
+                                <p className="flex text-xs ml-4 font-thin h-6 align-center">Road Transport</p>
+                            </div>
+                        </div>
+                        <div className="flex justify-center h-25 border-r pr-4"></div>
+                        <div className="w-[60%] ml-6">
+                            <p className="text-xs font-bold text-justify my-4">Care instructions to help you look after your pieces and keep them looking good for longer :</p>
+                            <p className="text-xs font-thin mt-4">Wash at or below 30°C (synthetics cycle)</p>
+                        </div>
+                    </div>
+
+                    )}
                 </div>
             </motion.div>
         </AnimatePresence>
